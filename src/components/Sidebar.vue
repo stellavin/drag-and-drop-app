eebc8d97ccb417d84fa4c910c1611e17682ea3d2
stellavin <stellavinmaris@gmail.com>
2025-01-22 14:16:57 +0300

    moved base layout and created sibar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <aside class="bg-left-column w-1/5 min-w-[250px] p-4 h-full">
+    <h2 class="text-white text-lg mb-4">{{ title }}</h2>
+    <div class="space-y-4">
+      <slot></slot>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    title: {
+      type: String,
+      default: 'Drag and Drop Elements',
+    },
+  },
+}
+</script>
+
+<style scoped>
+.bg-left-column {
+  background-color: var(--left-column);
+}
+</style>
